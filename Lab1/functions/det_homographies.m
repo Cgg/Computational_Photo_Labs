5fe55fa5c8ed6154f63b2e06fb868e1c17b5b7f7
Cgg <clement.geiger@gmail.com>
2011-09-08 12:31:05 +0200

    +algo

--- Lab1/functions/det_homographies.m
+++ Lab1/functions/det_homographies.m
@@ -17,3 +17,17 @@ function H = det_homographies(points1, points2)
 % FILL IN THIS PART
 %
 %------------------------------
+
+% Algorithm
+% foreach corresponding points i 
+%  compute alphai, betai
+%  stuff them at the end of AI, BI
+% endfor
+%
+% compute Q = assemblage de AI et BI
+%
+% we are looking for h so that Qh = 0
+%
+% apply a SVD to Q (svd(Q)) and get the solution
+%
+% put h into the 3x3 matrix H and return it
