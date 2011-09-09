d6bfcc256912c2087a8edfd5ff0755dc5c84b326
Cgg <clement.geiger@gmail.com>
2011-09-09 10:07:29 +0200

    ~refactoring

--- Lab1/functions/get_normalization_matrices.m
+++ Lab1/functions/get_normalization_matrices.m
@@ -1,23 +1,23 @@
-% function [norm_mat] = get_normalization_matrix(data);   
+% function [norm_mat] = get_normalization_matrix(data);
 %
-% Method: get all normalization matrices.  
-%         It is: point_norm = norm_matrix * point. The norm_points 
+% Method: get all normalization matrices.
+%         It is: point_norm = norm_matrix * point. The norm_points
 %         have centroid 0 and average distance = sqrt(2))
 % 
-% Input: data (3*m,n) (not normalized) the data may have NaN values 
+% Input: data (3*m,n) (not normalized) the data may have NaN values
 %        for m cameras and n points 
-%        
-% Output: norm_mat is a 3*m,3 matrix, which consists of all 
-%         normalization matrices matrices, i.e. norm_mat(1:3,:) is the 
-%         matrix for camera 1 
+%
+% Output: norm_mat is a 3*m,3 matrix, which consists of all
+%         normalization matrices matrices, i.e. norm_mat(1:3,:) is the
+%         matrix for camera 1
 %
 
 function [norm_mat] = get_normalization_matrices(data)
 
 
-% get Info 
-am_cams = size(data,1)/3;  
-am_points = size(data,2);   
+% get Info
+am_cams = size(data,1)/3;
+am_points = size(data,2);
 
 %------------------------------
 %
@@ -25,12 +25,7 @@ am_points = size(data,2);
 %
 %------------------------------
 
-% as a first test 
+% as a first test
 for hi1 = 1:am_cams
   norm_mat(hi1*3-2:hi1*3,:) = eye(3);
 end
-
-
-
-
-
