ade85a8f4bdc7dadb3e0cf4162fbbc0d551ca5b8
Cgg <clement.geiger@gmail.com>
2011-09-09 11:34:31 +0200

    +get_normalization matrices 1st implementation

--- Lab1/functions/get_normalization_matrices.m
+++ Lab1/functions/get_normalization_matrices.m
@@ -25,7 +25,59 @@ am_points = size(data,2);
 %
 %------------------------------
 
-% as a first test
-for hi1 = 1:am_cams
-  norm_mat(hi1*3-2:hi1*3,:) = eye(3);
+% Algo
+% for each camera do
+%  calculate distance
+%  calculate centroid
+%  calculate N
+%  put N in norm_mat
+
+N = eye( 3 );
+
+centroid = zeros( 3, 1 );
+distance = 0;
+
+sq2dist  = 0;
+
+n_points = 0;
+
+for i = 1 : am_cams
+
+  % centroid
+  for p = 1 : am_points
+
+    if isnan( data( i*3 - 2, p ) ) == false
+      centroid = centroid + data( i*3 - 2 : i*3, p );
+
+      % increase number of points taken into account
+      n_points = n_points + 1;
+    end
+  end
+
+  centroid = centroid / n_points;
+
+  % distance
+  for p = 1 : am_points
+
+    if isnan( data( i*3 - 2, p ) ) == false
+      distance = distance + sqrt( sum( ( data( i*3 - 2 : i*3, p ) - ...
+      centroid ).^2 ) );
+      % do not need to calculate n_points, already done
+    end
+  end
+
+  distance = distance / n_points;
+
+  n_points = 0;
+
+  % now compute N
+  sq2dist = sqrt( 2 ) / distance;
+
+  N( 1, 1 ) = sq2dist;
+  N( 2, 2 ) = sq2dist;
+
+  N( 1, 3 ) = - centroid( 1 ) * sq2dist;
+  N( 2, 3 ) = - centroid( 2 ) * sq2dist;
+
+  norm_mat( i*3 - 2 : i*3, : ) = N;
 end
