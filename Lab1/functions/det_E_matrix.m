49a7989209ae54a6358a41c9c7161185d734d339
Cgg <clement.geiger@gmail.com>
2011-09-10 09:34:09 +0200

    + implementation for determination of Essential matrix

--- Lab1/functions/det_E_matrix.m
+++ Lab1/functions/det_E_matrix.m
@@ -22,3 +22,37 @@ function E = det_E_matrix(points1, points2, K1, K2)
 % FILL IN THIS PART
 %
 %------------------------------
+
+E = zeros( 3 );
+
+% assuming points1 and points2 are paired
+% assuming points1 = pointsA and points2 = pointsB
+
+am_pts = size( points1, 2 );
+
+assert( am_pts == size( points2, 2 ), 'Must provide same number of points in each set' );
+
+e = ones( 1, 9 );
+
+W = zeros( am_pts, 9 );
+
+for i = 1 : am_pts
+  e( 1 ) = points2( 1 ) * points1( 1 );
+  e( 2 ) = points2( 1 ) * points1( 2 );
+  e( 3 ) = points2( 1 );
+  e( 4 ) = points2( 2 ) * points1( 1 );
+  e( 5 ) = points2( 2 ) * points1( 2 );
+  e( 6 ) = points2( 2 );
+  e( 7 ) =                points1( 1 );
+  e( 8 ) =                points1( 2 );
+
+  W( i, : ) = e;
+end
+
+[ U, S, V ] = svd( W );
+
+e = V( :, size( W, 2 ) )';
+
+E = [ e( 1:3 ) ; e( 4:6 ) ; e( 7:9 ) ];
+
+end
