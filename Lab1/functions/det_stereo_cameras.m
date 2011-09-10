a243f24f10e217d7706d8e485a6633988c5e4dba
Cgg <clement.geiger@gmail.com>
2011-09-10 13:03:32 +0200

    - useless cell array

--- Lab1/functions/det_stereo_cameras.m
+++ Lab1/functions/det_stereo_cameras.m
@@ -33,14 +33,14 @@ cam_centers( 4, : ) = 1; % homogeneous coordinate
 
 [ U, S, V ] = svd( E );
 
-% translation
+% translation of camera B
 T = V( :, 3 );
 
 T = T / norm( T );
 
 cam_centers( 1:3, 2 ) = T;
 
-% rotation
+% rotation of camera B
 Ma = [ K1 zeros( 3, 1 ) ];
 
 W = [ 0 -1  0 ; ...
@@ -68,17 +68,16 @@ Mb{ 2 } = [ ( K2 * R{1} ) -T ];
 Mb{ 3 } = [ ( K2 * R{2} )  T ];
 Mb{ 4 } = [ ( K2 * R{2} ) -T ];
 
-P = cell( 1, 4 );
-
 for i = 1 : 4
   % reconstruct 1st point of data for each possible Mb matrix
-  P{ i } = det_model( [ Ma ; Mb{ i } ], data( :, 1 ) )
+  P = det_model( [ Ma ; Mb{ i } ], data( :, 1 ) )
 
   % check for each point if it is in front of both cameras
-  % ie dot product btw camera z axis and vector from projection center to
+  % ie dot product btw camera z axis (given by 3rd column of associated
+  % rotation matrix) and vector from projection center to
   % point is 0.
-  CaP = P{ i }(1:3);
-  CbP = P{ i }(1:3) - Mb{ i }( :, 4 );
+  CaP = P(1:3);
+  CbP = P(1:3) - Mb{ i }( :, 4 );
 
   if( dot( CaP, Ma( :, 3 ) ) >= 0 && ...
       dot( CbP, R{ ceil( i / 2 ) }( :, 3 ) >= 0 )
