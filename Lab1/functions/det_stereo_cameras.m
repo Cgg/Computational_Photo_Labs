4390b4098f5cd5055ac50ee0ea4497705a887f10
Cgg <clement.geiger@gmail.com>
2011-09-10 12:52:19 +0200

    + 1st implementation of det_stereo_cam

--- Lab1/functions/det_stereo_cameras.m
+++ Lab1/functions/det_stereo_cameras.m
@@ -25,3 +25,69 @@ function [cams, cam_centers] = det_stereo_cameras(E, K1, K2, data)
 % FILL IN THIS PART
 %
 %------------------------------
+
+cams        = zeros( 6, 4 );
+cam_centers = zeros( 4, 2 );
+
+cam_centers( 4, : ) = 1; % homogeneous coordinate
+
+[ U, S, V ] = svd( E );
+
+% translation
+T = V( :, 3 );
+
+T = T / norm( T );
+
+cam_centers( 1:3, 2 ) = T;
+
+% rotation
+Ma = [ K1 zeros( 3, 1 ) ];
+
+W = [ 0 -1  0 ; ...
+      1  0  0 ; ...
+      0  0  1 ]
+
+R = cell( 1, 2 );
+
+R{1} = U * W  * V';
+R{2} = U * W' * V';
+
+if( det( R{1} ) < 0 )
+  R{1} = R{1} * ( - 1 );
+end
+
+if( det( R{2} ) < 0 )
+  R{2} = R{2} * ( - 1 );
+end
+
+% the 4 possible solutions
+Mb = cell( 1, 4 );
+
+Mb{ 1 } = [ ( K2 * R{1} )  T ];
+Mb{ 2 } = [ ( K2 * R{1} ) -T ];
+Mb{ 3 } = [ ( K2 * R{2} )  T ];
+Mb{ 4 } = [ ( K2 * R{2} ) -T ];
+
+P = cell( 1, 4 );
+
+for i = 1 : 4
+  % reconstruct 1st point of data for each possible Mb matrix
+  P{ i } = det_model( [ Ma ; Mb{ i } ], data( :, 1 ) )
+
+  % check for each point if it is in front of both cameras
+  % ie dot product btw camera z axis and vector from projection center to
+  % point is 0.
+  CaP = P{ i }(1:3);
+  CbP = P{ i }(1:3) - Mb{ i }( :, 4 );
+
+  if( dot( CaP, Ma( :, 3 ) ) >= 0 && ...
+      dot( CbP, R{ ceil( i / 2 ) }( :, 3 ) >= 0 )
+    fprintf( 'the solution is %i \n', i );
+    break % ugly
+  end
+end
+
+cams( 1:3, : ) = Ma;
+cams( 4:6, : ) = Mb{ i };
+
+end
