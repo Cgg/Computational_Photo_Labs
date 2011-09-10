19d656b9d4d80d42fa47079ae61bca3fbb1b5d5d
Cgg <clement.geiger@gmail.com>
2011-09-10 11:46:57 +0200

    -trailing spaces

--- Lab1/functions/det_model.m
+++ Lab1/functions/det_model.m
@@ -1,12 +1,12 @@
 % function model = det_model(cam, data)
 %
 % Method: Determines the 3D model points by triangulation
-%         of a stereo camera system. We assume that the data 
-%         is already normalized 
-% 
+%         of a stereo camera system. We assume that the data
+%         is already normalized
+%
 % Input: data (6,n) matrix
 %        cam (6,4) matrix
-% 
+%
 % Output: model (4,n matrix of all n points)
 %
 
