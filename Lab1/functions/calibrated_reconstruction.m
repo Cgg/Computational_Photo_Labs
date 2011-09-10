e6d3b3557ff0c1b29ceb8dca0367737d45ec6bc9
Cgg <clement.geiger@gmail.com>
2011-09-10 10:28:01 +0200

    -EOL spaces

--- Lab1/functions/calibrated_reconstruction.m
+++ Lab1/functions/calibrated_reconstruction.m
@@ -82,7 +82,7 @@ data_norm(4:6,:) = norm_mat(4:6,:) * data(4:6,:);
 E = det_E_matrix(data_norm(1:3,:), data_norm(4:6,:), K1, K2);
 
 % determine the two camera matrices
-[cams, cam_centers] = det_stereo_cameras(E, K1, K2, data(:,1)); 
+[cams, cam_centers] = det_stereo_cameras(E, K1, K2, data(:,1));
 
 % obtain the complete model by triangulation
 % first normalize the model and the cameras
@@ -92,21 +92,21 @@ cams_norm(4:6,:) = norm_mat(4:6,:) * cams(4:6,:);
 model = det_model(cams_norm, data_norm);
 
 % normalize the the 4th coordinate of model points to 1
-model = norm_points_to_one(model);  
+model = norm_points_to_one(model);
 
 % normalize the 4th coordinate of the camera centers to 1
-cam_centers = norm_points_to_one(cam_centers);    
+cam_centers = norm_points_to_one(cam_centers);
 
-% check the reprojection error 
+% check the reprojection error
 [error_average, error_max] = check_reprojection_error(data, cams, model);
 fprintf('\n\nThe reprojection error: data = cams * model is: \n');
-fprintf('Average error: %5.2fpixel; Maximum error: %5.2fpixel \n', error_average, error_max); 
+fprintf('Average error: %5.2fpixel; Maximum error: %5.2fpixel \n', error_average, error_max);
 
 %---------------------------------------------
 % Triangulation & Visualization & VRML model
 %---------------------------------------------
 
-% triangulate the result 
+% triangulate the result
 triang = get_delaunay_triang(data, image_ref);
 % triang = [];
 
